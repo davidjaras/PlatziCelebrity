49331bea6957080b0fd13813fbdf61aeb8698108
davidjaras <duvan318@gmail.com>
2020-11-05 14:38:03 -0500

    Scraper funcional. Validación de celebridades a partir de repositorio.
    Pendiente:
    Formatear datos para retornarlos en json
    Formatear fechas de noticias
    Implementación de argparse para uso de script por comandos
    Adecuar es script para búsquedas particulates

---
+++ scraper.py
@@ -0,0 +1,76 @@
+from common import config
+import functions
+
+
+def main_test():
+
+	# variables for testing
+	i_host = 4
+	i_notices_links = 0
+
+	# get site to scrape
+	host = config()['news_sites'][i_host]
+
+	# get links in site to scrape
+	notices_links = functions.get_links_notices_site(host)
+
+	# get links in search site to scrape
+	#notices_links = functions.get_links_notices_search(host)
+	
+	# print(notices_links)
+
+	# test get article elements
+	print(functions.scrape_article_from_link(
+		host, notices_links[i_notices_links]))
+
+
+def search_celebrity():
+
+	notices_links_count = 0
+	notices_articles = []
+
+	for i_host in range(1,4):
+		host = config()['news_sites'][i_host]
+		notices_links = functions.get_links_notices_search(host)
+		notices_links_count += len(notices_links)  # line for testing purpose
+
+		for i_notice_link in notices_links:
+			scraped_link = functions.scrape_article_from_link(host, i_notice_link)
+			if scraped_link:
+				notices_articles.append(scraped_link)
+
+	print(f'LINKS COUNT: {notices_links_count}')
+	print(f'ARTICLES SCRAPED COUNT: {len(notices_articles)}')
+	print(notices_articles)
+
+
+def main():
+
+	notices_links_count = 0
+	notices_articles = []
+
+	for i_host in range(5):
+		host = config()['news_sites'][i_host]
+		notices_links = functions.get_links_notices_site(host)
+		notices_links_count += len(notices_links) #line for testing purpose
+
+		for i_notice_link in notices_links:
+			scraped_link = functions.scrape_article_from_link(host, i_notice_link)
+			if scraped_link:
+				notices_articles.append(scraped_link)
+	
+	print(f'LINKS COUNT: {notices_links_count}')
+	print(f'ARTICLES SCRAPED COUNT: {len(notices_articles)}')
+	print(notices_articles)
+
+	# David: guarda los objetos de los links y de las noticias scrapeadas para comparara después
+	# también recuerda formatear las fechas de las noticias
+	# también adecua el script para busquedas particulares
+	# y por ultimo implementa el arg parse para la llamada del scritp
+	# 
+		
+
+
+if __name__ == "__main__":
+	#main()
+	search_celebrity()
