d9508db624732981af45b7463f75a59b0c7c0b14
octaviocvargas <evo@DESKTOP-QJN3CDS.localdomain>
2020-11-05 00:24:29 -0500

    Adelante del home, Peoplenews

--- frontend/src/components/GuideComponent/GuideComponent.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import GuideComponent from './GuideComponent';
-
-test('renders learn react link', () => {
-  render(<GuideComponent />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
-});
