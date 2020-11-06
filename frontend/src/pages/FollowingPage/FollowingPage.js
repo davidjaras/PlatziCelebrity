5c8baec0709719dccf81bffa60294ba4cbfc17e3
Sebastian Rodriguez Marin - @Seromarin <rodriguez.marin.sebastian@gmail.com>
2020-11-06 02:55:41 -0500

    Following Page Completed

--- frontend/src/pages/FollowingPage/FollowingPage.js
+++ frontend/src/pages/FollowingPage/FollowingPage.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import Bookmarks from './Bookmarks'
 import Celebrities from './Celebrities'
 
+import './styles/FollowingPage.scss'
+
 const FollowingPage = () => {
+
+    // let switcher = 'bookmarks';
+    const [switcher, setSwitcher] = useState('celebrities');
+
+    function switchTab(evt, selected) {
+        evt.preventDefault();
+        setSwitcher(selected);
+        console.log('epa', selected, switcher)
+    }
+
     return (
         <div className="container">
-            <Celebrities />
-            <Bookmarks />
+            <div className="switcher">
+                <span className={`switcher__celebrities ${switcher === 'celebrities' ? 'active' : ''}`} onClick={(evt) => switchTab(evt, 'celebrities')}>Celebridades</span>
+                <span className={`switcher__bookmarks ${switcher === 'bookmarks' ? 'active' : ''}`} onClick={(evt) => switchTab(evt, 'bookmarks')}>Marcadores</span>
+            </div>
+            {
+                switcher === 'celebrities'
+                    ?
+                    <div className="container">
+                        <Celebrities />
+                    </div>
+                    :
+                    <div className="container">
+                        <Bookmarks />
+                    </div>
+            }
         </div>
     )
 }
