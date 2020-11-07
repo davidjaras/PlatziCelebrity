44a9b935e2a1d760b203b564736bb40ea252bf0a
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-06 20:11:06 -0500

    fixed bug in models/post.js

--- backend/models/post.js
+++ backend/models/post.js
@@ -3,15 +3,15 @@ const {db} = require('../store/database');
 async function query (id){
     try {
         let post = [];
+        let idCategory = [];
         const data = await db.any(`SELECT categories_id FROM users_categories WHERE user_id = $1`,[id]);
-        await data.map( async function (element){
-            try{
-             post.push(await dataPost(element.categories_id));
-            }catch{}
-            console.log(post);
+        data.map( function (element){
+            idCategory.push(element.categories_id);
         });
-        function dataPost (id){
-            return db.any(`SELECT DISTINCT users_categories.categories_id,
+        console.log(idCategory);
+        for (let i = 0; i <= idCategory.length; i++){
+            let value = idCategory[i];
+            post.push( await db.any(`SELECT DISTINCT users_categories.categories_id,
             post_categories.post_id, post.title, post.content, post.source, post.views_, post.date_, post.image
             FROM 
             users_categories INNER JOIN post_categories ON(users_categories.categories_id = post_categories.categories_id)
@@ -21,16 +21,12 @@ async function query (id){
             users_categories.categories_id,
             post_categories.post_id, post.title, post.content, post.source, post.views_, post.date_, post.image
             ORDER BY post.date_ DESC
-            LIMIT 1`,[id]);
-            
-        };
-    return {
-        post,
-    };
+            LIMIT 10`,[value]));
+        }
+    return post;
     }catch(error){
         console.error(error);
     }
-    
 }
 
 module.exports = {
