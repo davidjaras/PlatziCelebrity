b2bdde813804b79872db07968abacbcdaeb6de69
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-08 15:31:05 -0500

    database

--- backend/store/database.js
+++ backend/store/database.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const {USER, PASSWORD, HOST, PORT, DATABASE} = process.env;
+const {USER, PASSWORD, HOST, PORTDB, DATABASE} = process.env;
 const db = pgp(`postgres://${USER}:${PASSWORD}@${HOST}:${PORTDB}/${DATABASE}`);
 
 function connect(){ 
