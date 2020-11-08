6670769cd0344d186db27ce62c91dbc91df74eaf
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-08 15:46:46 -0500

    port and host

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT_SERV;
+const PORT = process.env.PORT_SERV || 5000;
+const HOST = 'O.O.O.O'
 //controllers
 const registerController = require('./controllers/register');
 const postController = require('./controllers/post');
@@ -23,7 +24,7 @@ app.use('/profile', profileController);
 app.use('/register', registerController);
 app.use('/home', postController);
 
-app.listen( PORT,'0.0.0.0',function () {
-    console.log(`Server running in http://Localhost:${PORT}`);
+app.listen( PORT,HOST,function () {
+    console.log(`Server running `);
 });
 
