3c298567b600db358614c2ff2d7d1025fde19516
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-09 01:26:52 -0500

    try y

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,17 @@ app.use(function(req, res, next){
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 app.use(bodyParser.json());
 
 
 //Routes
-app.use(express.static(path.join(__dirname, '../frontend/public')))
+app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/public'))
+});
 app.use('/scrapper', scrapperController);
 app.use('/login', loginController);
 app.use('/profile', profileController);
