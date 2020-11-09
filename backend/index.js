368d7dd0d14ab91408c73555093898832d515917
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-09 00:55:09 -0500

    try

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const postController = require('./controllers/post');
 const profileController = require('./controllers/profile');
 const loginController = require('./controllers/login');
 const scrapperController = require('./controllers/scrapper');
-const index = require('../frontend/public/index');
+const index = require('../frontend/src/App');
 
 
 app.use(function(req, res, next){
