557290e4f827ab8696e1c0c47b4925d0091a9fe7
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-09 03:02:03 -0500

    t

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,16 @@ app.use(function(req, res, next){
 });
 
 //Routes
-app.use(express.static(path.join(__dirname, './frontend/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
-});
 app.use('/api/scrapper', scrapperController);
 app.use('/api/login', loginController);
 app.use('/api/profile', profileController);
 app.use('/api/register', registerController);
 app.use('/api/home', postController);
+
+app.use(express.static(path.join(__dirname, './frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
+});
 
 app.listen( process.env.PORT || 5000 ,'0.0.0.0',  function (){
     console.log("running in http://'0.0.0.0:",process.env.PORT)
