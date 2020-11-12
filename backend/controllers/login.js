1fe898535632df4951ba3fb5b3a5cc81a426182d
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-12 13:22:11 -0500

    changed body-parser

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -4,6 +4,7 @@ const loginModel = require('../models/login');
 
 router.post('/', async function(req, res) {
    const body = req.body;
+    console.log(body);
     const result = await loginModel.login(body);
     res.status(result.status).json(result);
   });
