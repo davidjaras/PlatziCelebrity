3af0ad3b547d16bb4b5b6716a39ecf9ee8da0324
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-12 11:48:35 -0500

    change cors

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,8 @@ const scrapperController = require('./controllers/scrapper');
 
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('content-type: application/json; charset=utf-8');
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //res.header('content-type: application/json; charset=utf-8');
     next();
 });
 
