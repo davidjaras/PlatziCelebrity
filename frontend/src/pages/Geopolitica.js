4c66d3e86848bcbc4c3cc96afc0dd523e7fcc773
OCTAVIO CUELLAR VARGAS <octaviocvargas@gmail.com>
2020-11-13 02:11:16 -0500

    Finish category

--- frontend/src/pages/Geopolitica.js
+++ frontend/src/pages/Geopolitica.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import {useState,useEffect} from 'react';
+import News from '../components/News';
+
+import { useHistory } from 'react-router-dom';
 
 function Geopolitica() {
+
+  const history = useHistory();
+
+  const [state,setState]= useState({news:[]})
+  useEffect(()=>{
+    consultNews()
+    const userSession = JSON.parse(sessionStorage.getItem('userSession'))
+      if (!userSession) {
+          history.push('/')
+      }
+  }, []);
+
+  let consultNews = async () => {
+    let url = 'https://peoplenews.herokuapp.com/api/home/category';
+
+
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ category : 3 }),
+      headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+    .then(respuesta => {
+      return respuesta.json();
+    })
+    .then(news => {
+      console.dir( news)
+      setState(news)
+
+    })
+  }
+
   return (
-    <div className='geopolitica'>
-      <h1>Geopolitica</h1>
+    <div className='inicio'>
+      <News news={state.post} />
     </div>
   );
 }
