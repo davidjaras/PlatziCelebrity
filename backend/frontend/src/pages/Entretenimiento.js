b9079d8e89657eb1f9ee9f7688470275bc84447a
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-11-13 12:09:17 -0500

    integration of categories page

--- backend/frontend/src/pages/Entretenimiento.js
+++ backend/frontend/src/pages/Entretenimiento.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import {useState,useEffect} from 'react';
+import News from '../components/News';
+
+import { useHistory } from 'react-router-dom';
 
 function Entretenimiento() {
+
+  const history = useHistory();
+
+  const [state,setState]= useState({news:[]})
+  useEffect(()=>{
+    consultNews()
+    const userSession = JSON.parse(sessionStorage.getItem('userSession'))
+      if (!userSession) {
+          history.push('/')
+      }
+  }, []);
+
+  let consultNews = async () => {
+    let url = 'https://peoplenews.herokuapp.com/api/home/category';
+
+
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ category : 1 }),
+      headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+    .then(respuesta => {
+      return respuesta.json();
+    })
+    .then(news => {
+      console.dir( news)
+      setState(news)
+
+    })
+  }
+
   return (
-    <div className='entretenimiento'>
-      <h1>Entretenimiento</h1>
+    <div className='inicio'>
+      <News news={state.post} />
     </div>
   );
 }
